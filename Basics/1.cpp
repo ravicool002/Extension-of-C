c54468fb5f99b44438c1f4e8f7ede18cc2f4fff8
Ravi Shankar Yadav <154975304+ravicool002@users.noreply.github.com>
2025-08-26 07:18:24 +0530

    Add files via upload

---
+++ Basics/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello World !"<< "\n"; //print string text written inside double quotes(""). 
+
+    cout<<2<< "\n" << endl; /*print integer value.
+    for new line '\n' and 'endl' both correct. (multiline comment-->/*.....*/
+        cout<<2*10 << endl;
+
+    cout<<"my self ravi\tyadav"<<endl; //horizontal tab 
+
+    cout<<"my self ravi \\yadav"<<endl; // for use backslash.(o/p-->my self ravi \yadav)
+
+    cout<<"my self ravi \"yadav"<<endl; //for double quote use..
+    return 0;
+}
